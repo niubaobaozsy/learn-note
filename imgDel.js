879f35c9892288085812778c4512945bc67d532c
zousy1 <zousy1@meicloud.com>
2020-10-22 17:54:40 +0800

    del img

--- imgDel.js
+++ imgDel.js
@@ -3,15 +3,21 @@ const fs = require('fs')
 var glob = require("glob")
 
 glob("**/imge/*.png", function (er, files) {
-  console.log(files)
   files.forEach(file => {
-    let img = file.split('/').pop()
-    // git grep 命令会执行 perl 的正则匹配所有满足冲突条件的文件
+    let path = file.split('/')
+    let img = path.pop()
+    path.pop()
+    let url = path.join('/')
+    console.log(url)
+    console.log(file)
+    // git grep 命令会执行 perl 的正则匹配所有满足冲突条件的文件,  用node进程执行命令，返回的结果是0，1如何拿到git show里面打印的内容？
     try {
-      results = execSync(`git grep -n "${img}"`, { encoding: 'utf-8' })
+      results = execSync(`git grep -r "${img}" "${url}"`, { encoding: 'utf-8' })
+      // 0 1
     } catch (e) {
       fs.unlinkSync(`./${file}`);
       console.log(file)
     }
   })
+  // git show
 })
